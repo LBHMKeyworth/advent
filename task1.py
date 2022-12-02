2b763923d09b9ea1e11208a71d5230c6d851ef02
LBHMkeyworth <matthew.keyworth@hackney.gov.uk>
2022-12-02 11:22:46 +0000

    Task 1

---
+++ task1.py
@@ -0,0 +1,21 @@
+import pprint
+
+all_elves = []
+
+with open('task1Input.txt', 'r') as f:
+    for line in f.readlines():
+        all_elves.append(line.replace("\n",""))
+calories = 0
+elf_totals = []
+for snack in all_elves:
+    if snack != "":
+        calories += int(snack)
+    else:
+        elf_totals.append(calories)
+        calories = 0
+
+
+#pprint.pprint(sorted(elf_totals))
+#get top 3 elves
+pprint.pprint(sorted(elf_totals, reverse=True)[:3])
+pprint.pprint(sum(sorted(elf_totals, reverse=True)[:3]))
